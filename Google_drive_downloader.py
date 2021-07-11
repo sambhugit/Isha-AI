9c8fde60e741f57006f16626900ff8a8bb5ebcda
Sambhu Nampooothiri G <67463941+sambhugit@users.noreply.github.com>
2021-07-11 09:25:02 +0530

    Update Google_drive_downloader.py

--- Google_drive_downloader.py
+++ Google_drive_downloader.py
@@ -5,6 +5,6 @@ current_dir = os.path.dirname(os.path.realpath('app.py'))
 gdd.download_file_from_google_drive(file_id='1-u5Wd0JJ9JA2YQ0u-G-DcH8G7ExgveYX',
                                     dest_path= os.path.join(current_dir,'sentiment_weight_file.hdf5'),
                                     unzip=False)
-gdd.download_file_from_google_drive(file_id='1HCz-2Mp9LT0-7ObImTEwXH-zcVpCwy8-',
+gdd.download_file_from_google_drive(file_id='1ASL3uv1wIuw3eLJWggNQzh2rErYr0LtE',
                                     dest_path= os.path.join(current_dir,'Emotion_detector.hdf5'),
                                     unzip=False)
